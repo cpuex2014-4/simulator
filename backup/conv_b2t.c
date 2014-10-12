efcda33ddbd0cd6c9763e4e31339aa3d7e632d3c
Schnee <schnee@schnee-E5410.(none)>
2014-10-13 06:40:21 +0900

    message

--- backup/conv_b2t.c
+++ backup/conv_b2t.c
@@ -4,24 +4,12 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define BUFF 65536
-#define LINE 2048
+#define BUFF 65536u
+#define LINE (65536u / 4u)
 
 
-/*
-unsigned int checkSize(unsigned int* byteCode) {
-	unsigned int size;
-	unsigned int i;
-
-	for(i = 0; i < LINE; i++) {
-		if(byteCode[i] != 0u) {
-			size = i+1;
-		}
-	}
-	return size;
-}
-*/
 unsigned int pow2 (unsigned int j) {
 	unsigned int retValue = 1;
 	while (j > 0) {
@@ -32,37 +20,36 @@ unsigned int pow2 (unsigned int j) {
 }
 /* 引数無し */
 int main (int argc, char* argv[]) {
-	int readCount = 0, writeCount = 0;
-	unsigned char readBuff[LINE] = { 0 };
+	unsigned int readCount = 0, writeCount = 0;
+	unsigned int countBuff = 0;
+	unsigned char readBuff[BUFF] = { 0 };
 	unsigned int byteCode[LINE] = { 0u };
-	unsigned int byteTemp = 0;
-	int bitCount = 0, line = 0;
 	unsigned int bitLoc = 0;	// 0<bitLoc<31
 	unsigned char textBuff[BUFF];
-	unsigned int byteSize;
 	unsigned int i, j, temp;
 
 	/* readBuff初期化 */	/* byteCode初期化 */
 	for (j=0; j<(LINE); j++) {
-		readBuff[j] = 0;
 		byteCode[j] = 0;
 	}
 	/* textBuff初期化 */
 	for (j=0; j<(BUFF); j++) {
+		readBuff[j] = 0;
 		textBuff[j] = 0;
 	}
 
 
 //	sleep(1);
-	do { /*EOFに到達していない*/
+//	do { /*EOFに到達していない*/
 
 /* バイナリコードをファイルから読み込んでreadBuffに書き込む */
 /* ファイルには'[0-4294967295].'と言う形で配置されている */
 /* EOFまで読み込む */
-		readCount = read(0, readBuff, BUFF);
-//		printf("[ DEBUG ]\treadCount = %u\n", readCount);
+		readCount = read(0, readBuff, BUFF);	// readCount [Byte=文字]だけ読み込む(最大65536bytes:unsigend int 16384文字)
+		countBuff = readCount;
+		printf("[ DEBUG ]\treadCount = %u\n", readCount);
 		j = 0;
-		while(i<LINE) {
+		while(i<readCount) {
 			temp = i % 4;
 			switch (temp) {
 				case 0: {
@@ -80,6 +67,7 @@ int main (int argc, char* argv[]) {
 				case 3: {
 					byteCode[j] =  byteCode[j] | (readBuff[i] << 24);
 					j++;
+//					printf("%u ", j);
 					break;
 				}
 				default: {
@@ -90,11 +78,10 @@ int main (int argc, char* argv[]) {
 			i++;
 		}
 
-		bitCount = 0;
-		byteTemp = 0;
+
 		i = 0;
 		temp = 0;
-		while(i < LINE) {
+		while(i < readCount/4) {
 			j = 0;
 			for(bitLoc = 31; bitLoc >= 0; bitLoc--) {
 				if( (byteCode[temp] % 2) > 0 ) {
@@ -102,32 +89,33 @@ int main (int argc, char* argv[]) {
 				} else {
 					textBuff[(bitLoc+i)] = '0';
 				}
+//				if((j+1)%32 == 0) printf("(%u, %u), ", i, j);
 				byteCode[temp] = byteCode[temp] / 2;
 				j++;
-				if(j==32) {
-					
+				if(j==33) {
 					break;
 				}
 			}
+			textBuff[(bitLoc+1+i)] = '\n';	
 			i = i + 32;
 			temp = i / 32;
 		}
-
+//		printf("\n");
 /* 数字列を標準出力に書き出す */
 /* ファイルには'0011001100110011001100110011001111011101110111011101110111011101'と言う形で配置される */
 
-//		byteSize = sizeof(textBuff);
-
-		for(i=0;i<BUFF;i++) {
-			if(textBuff[i]!=0) byteSize = i;
-		}
-
-			writeCount = write(1, textBuff, byteSize);
-			if(writeCount < 0) break;
-			byteSize = byteSize - writeCount;
-		line = 0;
-	} while (readCount == BUFF);
-
+		printf("%s\n", textBuff);
+		
+//		printf("[ DEBUG ]\treadCount = %u\n", readCount);
+/*
+			writeCount = write(1, textBuff, readCount);
+			if(writeCount < 1) break;
+			readCount = readCount - writeCount;
+		printf("\nreadCount = %u\n", readCount);
+*/
+//		sleep(4);
+//	} while (countBuff == BUFF);
+//		printf("[ DEBUG ]\tRETURN\n");
 	return 1;	// 正常終了時１を返す
 }
 
