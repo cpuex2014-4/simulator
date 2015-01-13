0abf4beac973cd4fbadf4c4fbf4c93be5b31262f
satoshi-1115 <litze128@gmail.com>
2015-01-13 15:52:51 +0900

    Add nmadd.s/nmsub.s

--- fpusub.c
+++ fpusub.c
@@ -9,11 +9,10 @@
 #include "fpusub.h"
 #define SHOWFLGCHK	(flag[HIDEIND] == 1)
 
-
-
-
-
-
+uint32_t fneg (uint32_t a) {
+  uint32_t nega = (1<<31)^a;
+  return nega;
+}
 
 unsigned int fmadds(unsigned int fr, unsigned int fs, unsigned int ft) {
 	unsigned int fd;
@@ -26,6 +25,21 @@ unsigned int fmsubs(unsigned int fr, unsigned int fs, unsigned int ft) {
 	unsigned int fd;
 	/* 積の丸め？ */
 	fd = fsub(fmul(fr, fs), ft);
+
+	return fd;
+}
+
+unsigned int nfmadds(unsigned int fr, unsigned int fs, unsigned int ft) {
+	unsigned int fd;
+	/* 積の丸め？ */
+	fd = fneg(fadd(fmul(fr, fs), ft));
+
+	return fd;
+}
+unsigned int nfmsubs(unsigned int fr, unsigned int fs, unsigned int ft) {
+	unsigned int fd;
+	/* 積の丸め？ */
+	fd = fneg(fsub(fmul(fr, fs), ft));
 
 	return fd;
 }
--- fpusub.h
+++ fpusub.h
@@ -2,6 +2,8 @@
 #define DEC_H
 
 
+uint32_t fneg (uint32_t a);
+
 unsigned int fmadds(unsigned int fr, unsigned int fs, unsigned int ft);
 unsigned int fmsubs(unsigned int fr, unsigned int fs, unsigned int ft);
 
